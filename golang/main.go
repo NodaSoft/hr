df04ac7cce1fdc6aa505783eb193111475df8247
Arthur <43719484+ArturSultanmagomedov@users.noreply.github.com>
2022-12-13 17:51:00 +0300

    Update main.go
    Ну тип готово)

--- golang/main.go
+++ golang/main.go
@@ -14,91 +14,77 @@ import (
 // приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
 // В конце должно выводить успешные таски и ошибки выполнены остальных тасков
 
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+// A Task represents the meaning of our life as a path, not as a goal
+type Task struct {
+	id       int
+	created  string // время создания
+	finished string // время выполнения
+	result   TaskResult
 }
 
+type TaskResult string
+
+// the less hardcode the better
+const (
+	SuccessTaskResult = "task has been successed"
+	FailTaskResult    = "something went wrong"
+)
+
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+	// send me feedback - t.me/kingarthurfish
+
+	tasksCount := 10 // in the best world we get it from config or something
+	tasks := make(chan Task)
+	done := make(chan struct{})
+	successedTasks := make([]any, 0)
+	failedTasks := make([]any, 0)
+
+	go taskCreator(tasks, tasksCount)
+	go taskWorker(tasks, done, &successedTasks, &failedTasks)
+
+	<-done // maybe better way is to use Context, but KISS is win there
+
+	fmt.Println("Errors:", failedTasks)
+	fmt.Println("Done tasks:", successedTasks)
+}
+
+func taskCreator(tasks chan<- Task, count int) {
+	for i := 0; i < count; i++ {
+		ft := time.Now().Format(time.RFC3339)
+		if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+			ft = "Some error occurred"
+		}
+		tasks <- Task{created: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+	}
+	close(tasks)
+}
+
+func taskWorker(tasks <-chan Task, done chan<- struct{}, st, ft *[]any) {
+	for {
+		task, ok := <-tasks
+		if !ok {
+			done <- struct{}{}
+			return
+		} else {
+			t := taskExecutor(task)
+			if t.result == SuccessTaskResult {
+				*st = append(*st, t.id)
+			} else {
+				*ft = append(*ft, fmt.Errorf("task id %d time %s, error %s", t.id, t.created, t.result))
 			}
-		}()
-	}
-
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
 		}
 	}
+}
 
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
-
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
-
-	time.Sleep(time.Second * 3)
-
-	println("Errors:")
-	for r := range err {
-		println(r)
+// in fact I have never got an error here lol
+func taskExecutor(task Task) Task {
+	tt, _ := time.Parse(time.RFC3339, task.created)
+	if tt.After(time.Now().Add(-20 * time.Second)) {
+		task.result = SuccessTaskResult
+	} else {
+		task.result = FailTaskResult
 	}
-
-	println("Done tasks:")
-	for r := range result {
-		println(r)
-	}
+	task.finished = time.Now().Format(time.RFC3339Nano)
+	time.Sleep(time.Millisecond * 150)
+	return task
 }
