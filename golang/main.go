b948dd905276cdd0d083f1bc127a36fccf268b48
ardasher-kh <48590262+ardasher-kh@users.noreply.github.com>
2022-12-16 20:54:40 +0500

    Task Emulator fixed and refactored

--- golang/main.go
+++ golang/main.go
@@ -2,103 +2,86 @@ package main
 
 import (
 	"fmt"
+	"strconv"
+	"sync"
 	"time"
 )
 
-// ЗАДАНИЕ:
-// * сделать из плохого кода хороший;
-// * важно сохранить логику появления ошибочных тасков;
-// * сделать правильную мультипоточность обработки заданий.
-// Обновленный код отправить через merge-request.
-
-// приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
-// В конце должно выводить успешные таски и ошибки выполнены остальных тасков
-
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+type Task struct {
+	Id int
+	CrtT,
+	FinT time.Time
+	Result  string
+	Success bool
 }
 
+func (t *Task) WorkTask() {
+	crtNano := t.CrtT.Nanosecond()
+	if crtNano%2 == 0 {
+		t.Result = "Task succeeded. Reason: " + strconv.Itoa(crtNano)
+		t.Success = true
+	} else {
+		t.Result = "Task failed. Reason: " + strconv.Itoa(crtNano)
+		t.Success = false
+	}
+	t.FinT = time.Now()
+	time.Sleep(time.Millisecond * 150)
+}
+
+func (t *Task) String() string {
+	return fmt.Sprintf("Task #%v took %v: %v", t.Id, t.FinT.Sub(t.CrtT), t.Result)
+}
+
+func genTasks(tCh chan Task, n int) {
+	for i := 0; i < n; i++ {
+		crT := time.Now()
+		tCh <- Task{Id: i, CrtT: crT}
+	}
+	close(tCh)
+}
+
+func workTasks(tCh, rCh chan Task) {
+	var wg sync.WaitGroup
+	for task := range tCh {
+		wg.Add(1)
+		go func(task Task) {
+			defer wg.Done()
+			task.WorkTask()
+			rCh <- task
+		}(task)
+	}
+	wg.Wait()
+	close(rCh)
+}
+
+const TasksCount = 20
+
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
-	}
 
-	superChan := make(chan Ttype, 10)
+	tasksCh := make(chan Task, 10)
+	taskResCh := make(chan Task, 10)
 
-	go taskCreturer(superChan)
+	go genTasks(tasksCh, TasksCount)
+	go workTasks(tasksCh, taskResCh)
 
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
+	succeededTasks := make([]*Task, 0, TasksCount)
+	failedTasks := make([]*Task, 0, TasksCount)
+
+	for task := range taskResCh {
+		task := task
+		if task.Success {
+			succeededTasks = append(succeededTasks, &task)
 		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+			failedTasks = append(failedTasks, &task)
 		}
 	}
 
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
-
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
-
-	time.Sleep(time.Second * 3)
-
-	println("Errors:")
-	for r := range err {
-		println(r)
+	fmt.Printf("Succeeded tasks(%v): \n", len(succeededTasks))
+	for _, sTask := range succeededTasks {
+		fmt.Println(sTask)
 	}
-
-	println("Done tasks:")
-	for r := range result {
-		println(r)
+	fmt.Printf("Succeeded tasks(%v): \n", len(failedTasks))
+	for _, fTask := range failedTasks {
+		fmt.Println(fTask)
 	}
 }
