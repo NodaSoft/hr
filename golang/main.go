a95279c234d9c285f000a9c79180094b6afce8c7
Aleksei Gubin <ag@denet.pro>
2023-01-22 12:08:23 +0600

    init refactoring

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"strings"
+	"sync/atomic"
 	"time"
 )
 
@@ -14,91 +17,144 @@ import (
 // приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
 // В конце должно выводить успешные таски и ошибки выполнены остальных тасков
 
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+// A Task represents a meaninglessness of our life
+type Task struct {
+	id           int64
+	creationTime string // время создания
+	finishedTime string // время выполнения
+	result       string
+}
+
+// TODO: can store complete tasks and failed tasks
+type taskManager struct {
+	id int64
+}
+
+func NewTaskManager() *taskManager {
+	return &taskManager{
+		id: 0,
+	}
+}
+
+func (t *taskManager) CreateTask(creationTime string) Task {
+	return Task{
+		id:           atomic.AddInt64(&t.id, 1),
+		creationTime: creationTime,
+	}
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
+	ctx, cancel := context.WithCancel(context.TODO())
+
+	taskManager := NewTaskManager()
+
+	taskWorkerChan := make(chan Task, 10)
+	taskResultChan := make(chan Task)
+
+	//creation workers pool
+	for i := 0; i < cap(taskWorkerChan); i++ {
+		go taskWorkers(ctx, taskWorkerChan, taskResultChan)
 	}
 
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
+	//start main work
 	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
+		defer func() {
+			close(taskWorkerChan)
+			close(taskResultChan)
+		}()
+
+		for {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				creationTime := time.Now().Format(time.RFC3339)
+				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+					creationTime = "Some error occured"
+				}
+
+				taskWorkerChan <- taskManager.CreateTask(creationTime) // передаем таск на выполнение
+			}
 		}
-		close(superChan)
 	}()
 
-	result := map[int]Ttype{}
-	err := []error{}
+	completeTasksChan := make(chan Task)
+	failedTasksChan := make(chan error)
+
+	taskSorter := func(task Task) {
+		if strings.Contains(task.result, "successed") {
+			completeTasksChan <- task
+		} else {
+			failedTasksChan <- fmt.Errorf("Task id %d time %s, error %s", task.id, task.creationTime, task.result)
+		}
+	}
+
+	//receiving result from workers pool
 	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
+		for task := range taskResultChan {
+			taskSorter(task)
 		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
+	}()
+
+	completeTasks := make(map[int64]Task)
+	errs := make([]error, 0)
+
+	go func() {
+		defer func() {
+			close(completeTasksChan)
+			close(failedTasksChan)
+		}()
+
+		for {
+			select {
+			case task := <-completeTasksChan:
+				completeTasks[task.id] = task
+			case err := <-failedTasksChan:
+				errs = append(errs, err)
+			case <-ctx.Done():
+				return
+			}
 		}
-		close(doneTasks)
-		close(undoneTasks)
 	}()
 
 	time.Sleep(time.Second * 3)
+	cancel()
 
-	println("Errors:")
-	for r := range err {
-		println(r)
+	fmt.Println("Errors:")
+	for _, err := range errs {
+		fmt.Println(err.Error())
 	}
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
+	fmt.Println("Done tasks:")
+	for id := range completeTasks {
+		fmt.Println(id)
 	}
+}
+
+func taskWorkers(ctx context.Context, workChan <-chan Task, result chan<- Task) {
+	for task := range workChan {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			result <- taskWorker(task)
+		}
+	}
+}
+
+func taskWorker(task Task) Task {
+	tt, _ := time.Parse(time.RFC3339, task.creationTime)
+
+	goal := time.Now().Add(-20 * time.Second)
+
+	task.result = "task has been successed"
+	if !tt.After(goal) {
+		task.result = "something went wrong"
+	}
+
+	task.finishedTime = time.Now().Format(time.RFC3339Nano)
+
+	time.Sleep(time.Millisecond * 150)
+
+	return task
 }
