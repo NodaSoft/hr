ef7d816e534c00f4aae4d04ba14695a7bb67ab98
Artem <ttion@mail.ru>
2023-02-05 23:33:23 +0300

    #feature-1111 - Переработка структуры программы на go hh

--- golang/main.go
+++ golang/main.go
@@ -3,16 +3,8 @@ package main
 import (
 	"fmt"
 	"time"
+	"runtime"
 )
-
-// ЗАДАНИЕ:
-// * сделать из плохого кода хороший;
-// * важно сохранить логику появления ошибочных тасков;
-// * сделать правильную мультипоточность обработки заданий.
-// Обновленный код отправить через merge-request.
-
-// приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
-// В конце должно выводить успешные таски и ошибки выполнены остальных тасков
 
 // A Ttype represents a meaninglessness of our life
 type Ttype struct {
@@ -22,75 +14,82 @@ type Ttype struct {
 	taskRESULT []byte
 }
 
+func taskCreturer (a chan Ttype) {
+    go func() {
+        for {
+            ft := time.Now().Format(time.RFC3339)
+            if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+                ft = "Some error occured"
+            }
+            a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+        }
+    }()
+}
+
+func task_worker (a Ttype) Ttype {
+    tt, _ := time.Parse(time.RFC3339, a.cT)
+    if tt.After(time.Now().Add(-20 * time.Second)) {
+        a.taskRESULT = []byte("task has been successed")
+    } else {
+        a.taskRESULT = []byte("something went wrong")
+    }
+    a.fT = time.Now().Format(time.RFC3339Nano)
+
+    time.Sleep(time.Millisecond * 150)
+
+    return a
+}
+
+func tasksorter (t Ttype, doneTasks chan Ttype, undoneTasks chan error) {
+    if string(t.taskRESULT[14:]) == "successed" {
+        doneTasks <- t
+    } else {
+        undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+    }
+}
+
+func getresulttask (doneTasks chan Ttype, undoneTasks chan error, result map[int]Ttype, err []error) {
+    for r := range doneTasks {
+        go func() {
+            result[r.id] = r
+        }()
+    }
+    for r := range undoneTasks {
+        go func() {
+            err = append(err, r)
+        }()
+    }
+}
+
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
-	}
-
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
+    runtime.GOMAXPROCS(4) //Колличество ядер для параллельного выполнения,а не конкурентного
 
 	result := map[int]Ttype{}
 	err := []error{}
+
+	superChan := make(chan Ttype, 10)
+	doneTasks := make(chan Ttype)
+	undoneTasks := make(chan error)
+
+	go taskCreturer(superChan)
+
 	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
+		// получение и обработка тасков
+		for t := range superChan {
+			t = task_worker(t)
+			go tasksorter(t, doneTasks, undoneTasks)
+			go getresulttask(doneTasks, undoneTasks,result,err)
 		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
+		close(superChan)
+        close(doneTasks)
+        close(undoneTasks)
 	}()
 
-	time.Sleep(time.Second * 3)
+    //Собственно ниже вывод результатов
+    //но можно дописать параллельные корутины по обработке этих
+    //тасков с условием в несколько потоков
+
+	time.Sleep(time.Second * 4)
 
 	println("Errors:")
 	for r := range err {
@@ -102,3 +101,4 @@ func main() {
 		println(r)
 	}
 }
+
