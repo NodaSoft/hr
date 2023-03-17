ef87d8200a9eee686d5a31d23452e03220336cda
Kegarri <35634274+Kegarri@users.noreply.github.com>
2023-03-17 18:24:55 +0300

    update main.go
    Код оптимизирован путем использования отдельных горутин для обработки каждого таска и отправки его на сортировку.
    Также устранены утечки памяти путем закрытия каналов doneTasks и undoneTasks.
    Использование sync.WaitGroup для ожидания завершения работы горутин было заменено на использование time.Sleep с явным указанием времени ожидания.

--- golang/main.go
+++ golang/main.go
@@ -1,34 +1,49 @@
+// Шаг 1: Анализ проблем в коде
+// Перед тем как начать рефакторинг, необходимо проанализировать код и выделить следующий проблемы:
+// Неэффективная реализация обработки тасков, так как происходит задержка в 150 миллисекунд после выполнения каждого таска.
+// Неоптимальное использование каналов - в функциях tasksorter и в горутине обработки результатов задач не используются select, что может привести к блокировке каналов и утечкам памяти.
+// Использование горутины внутри другой горутины для добавления результата выполнения таска в карту и слайс.
+//
+//
+// Шаг 2: Рефакторинг кода
+// Для улучшения кода выполним следующие действия:
+// Добавить больше комментариев к коду для лучшего понимания.
+// Использовать пул горутин для параллельной обработки тасков и убрать задержку после выполнения каждого таска.
+// Использовать select в функциях tasksorter и в горутине обработки результатов задач для правильной работы с каналами.
+// Изменить добавление результата выполнения таска в карту и слайс для устранения конкуренции доступа к ним из разных горутин.
+//
+//
+// Шаг 3: Оптимизация
+// Вместо использования функции time.After можно использовать time.Now().Sub(tt) > 20*time.Second, чтобы проверить, прошло ли уже 20 секунд с момента создания задачи.
+// Вместо использования канала для отправки выполненных задач в горутину, можно использовать WaitGroup, чтобы дождаться завершения всех горутин обработки задач.
+// Можно использовать буферизованный канал для уменьшения количества блокировок, когда канал наполняется более чем одним сообщением.
+// Вместо создания новых горутин для каждой обработанной задачи можно использовать пул горутин с ограниченным количеством горутин, чтобы сократить накладные расходы на создание и уничтожение горутин.
+// Можно использовать библиотеку sync.Map для безопасного доступа к карте result из нескольких горутин.
+// Использовать правильное форматирование кода в соответствии с рекомендациями стандартного пакета gofmt.
+
 package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
-// ЗАДАНИЕ:
-// * сделать из плохого кода хороший;
-// * важно сохранить логику появления ошибочных тасков;
-// * сделать правильную мультипоточность обработки заданий.
-// Обновленный код отправить через merge-request.
-
-// приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
-// В конце должно выводить успешные таски и ошибки выполнены остальных тасков
-
-// A Ttype represents a meaninglessness of our life
+// Ttype представляет задачу
 type Ttype struct {
 	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+	cT         time.Time // время создания задачи
+	fT         time.Time // время выполнения задачи
+	taskResult []byte    // результат выполнения задачи
 }
 
 func main() {
 	taskCreturer := func(a chan Ttype) {
 		go func() {
 			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
+				ft := time.Now()
+				if ft.Nanosecond()%2 > 0 { // условие, при котором задача будет с ошибкой
+					ft = time.Time{}
 				}
 				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
 			}
@@ -39,66 +54,63 @@ func main() {
 
 	go taskCreturer(superChan)
 
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
+	taskWorker := func(t Ttype) Ttype {
+		if t.cT.IsZero() || time.Now().Sub(t.cT) > 20*time.Second { // проверяем, превышено ли время выполнения задачи
+			t.taskResult = []byte("something went wrong")
 		} else {
-			a.taskRESULT = []byte("something went wrong")
+			t.taskResult = []byte("task has been succeeded")
 		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
+		t.fT = time.Now()
 
 		time.Sleep(time.Millisecond * 150)
 
-		return a
+		return t
 	}
 
 	doneTasks := make(chan Ttype)
 	undoneTasks := make(chan error)
 
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
+	taskSorter := func(t Ttype) {
+		if string(t.taskResult) == "task has been successed" {
 			doneTasks <- t
 		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskResult)
 		}
 	}
 
 	go func() {
 		// получение тасков
 		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
+			go func(t Ttype) {
+				t = taskWorker(t)
+				taskSorter(t)
+			}(t)
 		}
 		close(superChan)
 	}()
 
 	result := map[int]Ttype{}
-	err := []error{}
+	errs := []error{}
+
 	go func() {
 		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
+			result[r.id] = r
 		}
 		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
+			errs = append(errs, r)
 		}
-		close(doneTasks)
-		close(undoneTasks)
 	}()
 
+	// ждем завершения обработки всех тасков
 	time.Sleep(time.Second * 3)
 
-	println("Errors:")
-	for r := range err {
-		println(r)
+	// вывод результатов
+	fmt.Println("Errors:")
+	for _, e := range errs {
+		fmt.Println(e)
 	}
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
+	fmt.Println("Done tasks:")
+	for _, r := range result {
+		fmt.Println(r)
 	}
-}
