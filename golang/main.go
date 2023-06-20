a5b6701a0659a45ffa49f75dc1fcb57e77df7149
Shiro836 <gazikkamalov2@gmail.com>
2023-06-21 00:01:14 +0300

    initial commit

--- golang/main.go
+++ golang/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -17,88 +19,169 @@ import (
 // A Ttype represents a meaninglessness of our life
 type Ttype struct {
 	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+	createTime string // время создания
+	finishTime string // время выполнения
+
+	err error
 }
 
-func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+var taskId = &atomic.Int64{}
+
+func newTask() *Ttype {
+	now := time.Now()
+	ft := now.Format(time.RFC3339)
+
+	if now.Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+		ft = "Some error occured"
+	}
+	return &Ttype{createTime: ft, id: int(taskId.Add(1))}
+}
+
+func taskSpawner(done <-chan struct{}) <-chan *Ttype {
+	ch := make(chan *Ttype)
+
+	go func() {
+		defer close(ch)
+
+		for {
+			select {
+			case <-done:
+				return
+			case ch <- newTask(): // передаем таск на выполнение
 			}
+		}
+	}()
+
+	return ch
+}
+
+func processTask(task *Ttype) {
+	if tt, err := time.Parse(time.RFC3339, task.createTime); err != nil {
+		task.err = fmt.Errorf("time parse error: %w", err)
+
+		return
+	} else if !tt.After(time.Now().Add(-20 * time.Second)) {
+		task.err = fmt.Errorf("something went wrong")
+
+		return
+	}
+
+	task.finishTime = time.Now().Format(time.RFC3339Nano)
+
+	time.Sleep(time.Millisecond * 150) // work emulation
+}
+
+func taskWorker(tasks <-chan *Ttype) <-chan *Ttype {
+	ch := make(chan *Ttype)
+
+	go func() {
+		defer close(ch)
+
+		for task := range tasks {
+			processTask(task)
+
+			ch <- task
+		}
+	}()
+
+	return ch
+}
+
+func fanIn[T any](ch1, ch2 <-chan T) <-chan T {
+	ch := make(chan T)
+
+	go func() {
+		defer close(ch)
+
+		for {
+			if ch1 == nil && ch2 == nil {
+				return
+			}
+
+			select {
+			case val, ok := <-ch1:
+				if !ok {
+					ch1 = nil
+					break
+				}
+
+				ch <- val
+			case val, ok := <-ch2:
+				if !ok {
+					ch2 = nil
+					break
+				}
+
+				ch <- val
+			}
+		}
+	}()
+
+	return ch
+}
+
+func taskSorter(tasks <-chan *Ttype) (<-chan *Ttype, <-chan *Ttype) {
+	doneTasks, undoneTasks := make(chan *Ttype), make(chan *Ttype)
+
+	go func() {
+		defer func() {
+			close(doneTasks)
+			close(undoneTasks)
 		}()
-	}
 
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
+		for task := range tasks {
+			if task.err != nil {
+				undoneTasks <- task
+			} else {
+				doneTasks <- task
+			}
 		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
 	}()
 
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
+	return doneTasks, undoneTasks
+}
+
+const workersCnt = 5
+
+func main() {
+	done := make(chan struct{})
+
+	tasks := taskSpawner(done)
+
+	var taskResults <-chan *Ttype = nil
+	for i := 0; i < workersCnt; i++ {
+		taskResults = fanIn(taskResults, taskWorker(tasks))
+	}
+
+	doneTasks, undoneTasks := taskSorter(taskResults)
 
 	time.Sleep(time.Second * 3)
 
-	println("Errors:")
-	for r := range err {
-		println(r)
+	close(done)
+
+	errs := []error{}
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	// if we don't read one channel and sorter is trying to write to that inactive channel we will block
+
+	go func() {
+		for r := range undoneTasks {
+			errs = append(errs, r.err)
+		}
+		wg.Done()
+	}()
+
+	fmt.Println("Done tasks:")
+	for r := range doneTasks {
+		fmt.Println(r.id)
 	}
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
+	wg.Wait()
+
+	fmt.Println("Errors:")
+	for _, err := range errs {
+		fmt.Println(err)
 	}
 }
