7581465bbdd7327d486e284a137cf40901f37b4f
savukhin <savukhin@gmail.com>
2023-06-29 16:22:34 +0300

    Saveliy Karpukhin's code

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -14,91 +16,183 @@ import (
 // приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
 // В конце должно выводить успешные таски и ошибки выполнены остальных тасков
 
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+// Task - название нашей таски
+type Task struct {
+	id           int64
+	creationTime time.Time // время создания
+	finalTime    time.Time // время выполнения
+	taskRESULT   []byte
+	err          error
+}
+
+func createTasks(a chan Task) {
+	startTime := time.Now()
+
+	var wgCreator sync.WaitGroup
+	stopChan := make(chan bool)
+
+	wgCreator.Add(1)
+	go func(wg *sync.WaitGroup) {
+		cnt := 0
+
+		for {
+			isBreak := false
+			select {
+			case stop, ok := <-stopChan:
+				if ok && stop {
+					isBreak = true
+					break
+				}
+			default:
+				finalTime := time.Now()
+
+				nsecs := finalTime.Sub(startTime).Nanoseconds()
+
+				newTask := Task{creationTime: finalTime, id: time.Now().UnixNano() + nsecs}
+
+				// Костыль, так как мой компьютер (Macbook Pro 2019) округляет наносекунды до микросекунд при вызове UnixNano или time.Now().Nanosecond()
+				if nsecs%2 == 1 { // вот такое условие появления ошибочных тасков
+					newTask.err = errors.New("Some error occured")
+				}
+				a <- newTask // передаем таск на выполнение
+				cnt += 1
+			}
+
+			if isBreak {
+				break
+			}
+		}
+
+		fmt.Printf("Total generated tasks %d\n", cnt)
+		wg.Done()
+	}(&wgCreator)
+
+	time.Sleep(200 * time.Millisecond)
+	// time.Sleep(3 * time.Second)
+
+	stopChan <- true
+	wgCreator.Wait()
+
+}
+
+func (t *Task) processTask() {
+	tt := t.creationTime
+
+	// if tt.After(time.Now().Add(-20 * time.Second)) {
+	if time.Now().Sub(tt) < 20*time.Second && t.err == nil {
+		t.taskRESULT = []byte("task has been successed")
+	} else {
+		t.taskRESULT = []byte("something went wrong")
+	}
+	t.finalTime = time.Now()
+
+	// В исходном коде стоял sleep и при ошибке, и при успешной обработке. Так и надо было оставить, верно?
+	time.Sleep(time.Millisecond * 150)
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
-	}
+	superChan := make(chan Task, 10) // Super Tengen Toppa Guren Lagann? :) Оставляю изначальное название, по смыслу подходит
 
-	superChan := make(chan Ttype, 10)
+	doneTasks := make(chan Task, 10)
+	undoneTasks := make(chan Task, 10)
 
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
+	tasksorter := func(t Task) {
 		if string(t.taskRESULT[14:]) == "successed" {
 			doneTasks <- t
 		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+			undoneTasks <- t
 		}
 	}
 
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
+	// Почти те же мапы, только добавили мьютексов
+	var taskIDMutex sync.RWMutex
+	taskIDs := map[int64]Task{}
 
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
+	var errorsMutex sync.RWMutex
+	errorTaskIDs := map[int64]Task{} // Хочется хранить таски, а не строки
 
-	time.Sleep(time.Second * 3)
+	workers := 10
+	var wg sync.WaitGroup
+
+	for i := 0; i < workers; i++ {
+		wg.Add(1)
+
+		go func(wg *sync.WaitGroup, tasksChan chan Task, errorsChan chan Task) {
+			ch1Broken := false
+			ch2Broken := false
+
+			for {
+				select { // Одинаково обрабатываем оба канала, но пишем в разные мапы
+				case task, ok := <-tasksChan:
+					if !ok {
+						// Уровень вложенности 6 табов, оставляю из вредности к исходному коду, который надо было исправить
+						ch1Broken = true
+						break
+					}
+					taskIDMutex.Lock()
+					taskIDs[task.id] = task
+					taskIDMutex.Unlock()
+
+				case task, ok := <-errorsChan:
+					if !ok {
+						ch2Broken = true // Уровень вложенности по прежнему 6 табов
+						break
+					}
+					errorsMutex.Lock()
+					errorTaskIDs[task.id] = task
+					errorsMutex.Unlock()
+				}
+
+				if ch1Broken && ch2Broken {
+					break
+				}
+
+			}
+			wg.Done()
+
+		}(&wg, doneTasks, undoneTasks)
+	}
+
+	workersSuper := 10
+	var wgSuper sync.WaitGroup
+	for i := 0; i < workersSuper; i++ {
+		wgSuper.Add(1)
+
+		go func(wgSuper *sync.WaitGroup, ch chan Task) {
+			cnt := 0
+
+			// получение тасков
+			for t := range ch {
+				t.processTask()
+				tasksorter(t)
+
+				cnt += 1
+			}
+
+			wgSuper.Done()
+		}(&wgSuper, superChan)
+	}
+
+	createTasks(superChan)
+	close(superChan)
+
+	wgSuper.Wait() // Значит, воркеры, заполняющие doneTasks и undoneTasks отработали и эти каналы можно закрывать
+
+	close(doneTasks)
+	close(undoneTasks)
+
+	wg.Wait() // Ожидаем выполнение тасков (sleep на)
 
 	println("Errors:")
-	for r := range err {
-		println(r)
+	for _, t := range errorTaskIDs {
+		fmt.Printf("Task id %d time %s, error %s\n", t.id, t.creationTime.Format(time.RFC3339Nano), string(t.taskRESULT))
 	}
 
 	println("Done tasks:")
-	for r := range result {
-		println(r)
+	for _, t := range taskIDs {
+		fmt.Printf("Task id %d time %s, SUCCESS %s\n", t.id, t.creationTime.Format(time.RFC3339Nano), string(t.taskRESULT))
 	}
+
+	// Проверка, что количество изначально сгенерированных и обработанных тасков совпадает
+	fmt.Printf("Count of errors %d\nCount of success %d\nSum %d\n", len(errorTaskIDs), len(taskIDs), len(errorTaskIDs)+len(taskIDs))
 }
