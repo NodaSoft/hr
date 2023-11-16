bc309bd14f37c4f08ff4d3fc5fc1dded96457497
evanh-shumakov <evanh.shumakov@gmail.com>
2023-11-16 17:09:47 +0400

    phpstan: Method NodaSoft\Operation\Params\NotifyComplaintNewParams::getResellerId() should return int but returns int|null.

--- php/v2/src/Operation/Params/NotifyComplaintNewParams.php
+++ php/v2/src/Operation/Params/NotifyComplaintNewParams.php
@@ -7,37 +7,37 @@ use NodaSoft\Request\Request;
 class NotifyComplaintNewParams implements Params
 {
     /** @var ?int */
-    private $resellerId;
+    private $resellerId = null;
 
     /** @var ?int */
-    private $clientId;
+    private $clientId = null;
 
     /** @var ?int */
-    private $creatorId;
+    private $creatorId = null;
 
     /** @var ?int */
-    private $expertId;
+    private $expertId = null;
 
     /** @var ?int */
-    private $notificationType;
+    private $notificationType = null;
 
     /** @var ?int */
-    private $complaintId;
+    private $complaintId = null;
 
     /** @var ?string */
-    private $complaintNumber;
+    private $complaintNumber = null;
 
     /** @var ?int */
-    private $consumptionId;
+    private $consumptionId = null;
 
     /** @var ?string */
-    private $consumptionNumber;
+    private $consumptionNumber = null;
 
     /** @var ?string */
-    private $agreementNumber;
+    private $agreementNumber = null;
 
     /** @var ?string */
-    private $date;
+    private $date = null;
 
     public function setRequest(Request $request): void
     {
@@ -82,62 +82,62 @@ class NotifyComplaintNewParams implements Params
         return $array;
     }
 
-    public function getResellerId(): int
+    public function getResellerId(): ?int
     {
         return $this->resellerId;
     }
 
-    public function setResellerId(?int $resellerId): void
+    public function setResellerId(int $resellerId): void
     {
         $this->resellerId = $resellerId;
     }
 
-    public function getClientId(): int
+    public function getClientId(): ?int
     {
         return $this->clientId;
     }
 
-    public function setClientId(?int $clientId): void
+    public function setClientId(int $clientId): void
     {
         $this->clientId = $clientId;
     }
 
-    public function getCreatorId(): int
+    public function getCreatorId(): ?int
     {
         return $this->creatorId;
     }
 
-    public function setCreatorId(?int $creatorId): void
+    public function setCreatorId(int $creatorId): void
     {
         $this->creatorId = $creatorId;
     }
 
-    public function getExpertId(): int
+    public function getExpertId(): ?int
     {
         return $this->expertId;
     }
 
-    public function setExpertId(?int $expertId): void
+    public function setExpertId(int $expertId): void
     {
         $this->expertId = $expertId;
     }
 
-    public function getNotificationType(): int
+    public function getNotificationType(): ?int
     {
         return $this->notificationType;
     }
 
-    public function setNotificationType(?int $notificationType): void
+    public function setNotificationType(int $notificationType): void
     {
         $this->notificationType = $notificationType;
     }
 
-    public function getComplaintId(): int
+    public function getComplaintId(): ?int
     {
         return $this->complaintId;
     }
 
-    public function setComplaintId(?int $complaintId): void
+    public function setComplaintId(int $complaintId): void
     {
         $this->complaintId = $complaintId;
     }
@@ -147,47 +147,47 @@ class NotifyComplaintNewParams implements Params
         return $this->complaintNumber;
     }
 
-    public function setComplaintNumber(?string $complaintNumber): void
+    public function setComplaintNumber(string $complaintNumber): void
     {
         $this->complaintNumber = $complaintNumber;
     }
 
-    public function getConsumptionId(): int
+    public function getConsumptionId(): ?int
     {
         return $this->consumptionId;
     }
 
-    public function setConsumptionId(?int $consumptionId): void
+    public function setConsumptionId(int $consumptionId): void
     {
         $this->consumptionId = $consumptionId;
     }
 
-    public function getConsumptionNumber(): string
+    public function getConsumptionNumber(): ?string
     {
         return $this->consumptionNumber;
     }
 
-    public function setConsumptionNumber(?string $consumptionNumber): void
+    public function setConsumptionNumber(string $consumptionNumber): void
     {
         $this->consumptionNumber = $consumptionNumber;
     }
 
-    public function getAgreementNumber(): string
+    public function getAgreementNumber(): ?string
     {
         return $this->agreementNumber;
     }
 
-    public function setAgreementNumber(?string $agreementNumber): void
+    public function setAgreementNumber(string $agreementNumber): void
     {
         $this->agreementNumber = $agreementNumber;
     }
 
-    public function getDate(): string
+    public function getDate(): ?string
     {
         return $this->date;
     }
 
-    public function setDate(?string $date): void
+    public function setDate(string $date): void
     {
         $this->date = $date;
     }
