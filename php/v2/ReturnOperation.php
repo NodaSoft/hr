ec1aa7f9cad45e5f689d6f577cc55eaf536cfd81
Vitaly Filippov <kazanova84@gmail.com>
2023-11-21 20:02:18 +0300

    Test task by Vitaly Filippov

--- php/v2/ReturnOperation.php
+++ php/v2/ReturnOperation.php
@@ -10,11 +10,22 @@ class TsReturnOperation extends ReferencesOperation
     /**
      * @throws \Exception
      */
-    public function doOperation(): void
+    public function doOperation(): array
     {
-        $data = (array)$this->getRequest('data');
-        $resellerId = $data['resellerId'];
-        $notificationType = (int)$data['notificationType'];
+        /**
+         * @vphilippov:
+         * Вместо жесткого приведения типа к (array) лучше проверить тип праметра:
+         * так легче отлаживать код, если вместо массива будет передано что-то другое.
+         */
+        $data = $this->getRequest('data');
+
+        if (!is_array($data)) {
+            throw new \Exception('Parameter [data] is not specified or has invalid value');
+        }
+
+        $resellerId = (int)($data['resellerId'] ?? 0);
+        $notificationType = $data['notificationType'] ?? null;
+
         $result = [
             'notificationEmployeeByEmail' => false,
             'notificationClientByEmail'   => false,
@@ -24,59 +35,114 @@ class TsReturnOperation extends ReferencesOperation
             ],
         ];
 
-        if (empty((int)$resellerId)) {
-            $result['notificationClientBySms']['message'] = 'Empty resellerId';
+        if (empty($resellerId)) {
+            /**
+             * @vphilippov:
+             * Не понятно, почему здесь возврат результата при некорректном параметре, в то время как в остальных
+             * подобных случаях бросается Exception. Возможно такая стоит задача, но больше похоже на логическую ошибку.
+             * Вместо return стоит вставить throw new \Exception(...)
+             */
+            $result['notificationClientBySms']['message'] = 'Parameter [resellerId] is empty or has invalid value';
             return $result;
         }
 
-        if (empty((int)$notificationType)) {
-            throw new \Exception('Empty notificationType', 400);
+        if (empty($notificationType) || !in_array($notificationType, [self::TYPE_NEW, self::TYPE_CHANGE])) {
+            throw new \Exception('Parameter [notificationType] is empty or has invalid value', 400);
         }
 
-        $reseller = Seller::getById((int)$resellerId);
+        /**
+         * @vphilippov:
+         * Проверка результата getById на значение null имеет смысл только после доработки самого метода,
+         * который в своем первоначальном виде не мог возвращать что-то, кроме объекта типа Contractor.
+         *
+         * Где-то логическая ошибка: либо метод getById должен бросать исключение, когда объект не найден
+         * (тогда проверка результата на null не нужна), либо должен уметь возвращать null.
+         * Выбран второй вариант чтобы сообщения Exception были более содержательными.
+         */
+
+        $reseller = Seller::getById($resellerId);
         if ($reseller === null) {
             throw new \Exception('Seller not found!', 400);
         }
 
-        $client = Contractor::getById((int)$data['clientId']);
+        $client = Contractor::getById($data['clientId'] ?? 0);
         if ($client === null || $client->type !== Contractor::TYPE_CUSTOMER || $client->Seller->id !== $resellerId) {
-            throw new \Exception('сlient not found!', 400);
+            throw new \Exception('Client not found!', 400);
         }
 
-        $cFullName = $client->getFullName();
-        if (empty($client->getFullName())) {
-            $cFullName = $client->name;
-        }
+        /*
+         * @vphilippov:
+         * Выражение не имеет смысла, т.к. если пуст fullName, то name и подавно
 
-        $cr = Employee::getById((int)$data['creatorId']);
-        if ($cr === null) {
+            $cFullName = $client->getFullName();
+            if (empty($client->getFullName())) {
+                $cFullName = $client->name;
+            }
+        */
+
+        $creator = Employee::getById($data['creatorId'] ?? 0);
+        if ($creator === null) {
             throw new \Exception('Creator not found!', 400);
         }
 
-        $et = Employee::getById((int)$data['expertId']);
-        if ($et === null) {
+        $expert = Employee::getById($data['expertId'] ?? 0);
+        if ($expert === null) {
             throw new \Exception('Expert not found!', 400);
         }
 
+        $dataDifferences = $data['differences'] ?? [];  // expect array
+
+        if (!is_array($dataDifferences)) {
+            throw new \Exception('Parameter [differences] has invalid value');
+        }
+
+        if (isset($dataDifferences['from']) && !Status::checkId($dataDifferences['from'])) {
+            throw new \Exception('Parameter [differences][from] has invalid value');
+        }
+
+        if (isset($dataDifferences['to']) && !Status::checkId($dataDifferences['to'])) {
+            throw new \Exception('Parameter [differences][to] has invalid value');
+        }
+
         $differences = '';
+
+        /**
+         * @vphilippov:
+         * Функции __() в коде не объявлено, корректность вызова нельзя проверить.
+         * Судя по всему, имеется ввиду какой-то шаблонизатор, возвращающий строку.
+         * Поэтому исходим из того, что в вызовах этой функции нет ошибок
+         */
+
         if ($notificationType === self::TYPE_NEW) {
             $differences = __('NewPositionAdded', null, $resellerId);
-        } elseif ($notificationType === self::TYPE_CHANGE && !empty($data['differences'])) {
-            $differences = __('PositionStatusHasChanged', [
-                    'FROM' => Status::getName((int)$data['differences']['from']),
-                    'TO'   => Status::getName((int)$data['differences']['to']),
-                ], $resellerId);
+
+        } else if ($notificationType === self::TYPE_CHANGE && !empty($dataDifferences)) {
+            // Считаем, что свойства [from] и [to] являются обязательными.
+            // Корректность значений проверена выше
+            if (isset($dataDifferences['from']) && isset($dataDifferences['to'])) {
+                // Не лишним будет так же проверить, что данные действительно поменялись
+                if ($dataDifferences['from'] != $dataDifferences['to']) {
+                    $differences = __('PositionStatusHasChanged', [
+                        'FROM' => Status::getName($dataDifferences['from']),
+                        'TO'   => Status::getName($dataDifferences['to']),
+                    ],
+                        $resellerId
+                    );
+                }
+            } else {
+                throw new \Exception('Parameter [differences] must have [from] and [to] properties');
+            }
         }
 
         $templateData = [
             'COMPLAINT_ID'       => (int)$data['complaintId'],
             'COMPLAINT_NUMBER'   => (string)$data['complaintNumber'],
-            'CREATOR_ID'         => (int)$data['creatorId'],
-            'CREATOR_NAME'       => $cr->getFullName(),
-            'EXPERT_ID'          => (int)$data['expertId'],
-            'EXPERT_NAME'        => $et->getFullName(),
-            'CLIENT_ID'          => (int)$data['clientId'],
-            'CLIENT_NAME'        => $cFullName,
+            'CREATOR_ID'         => $creator->id,
+            'CREATOR_NAME'       => $creator->getFullName(),
+            'EXPERT_ID'          => $expert->id,
+            'EXPERT_NAME'        => $expert->getFullName(),
+            'CLIENT_ID'          => $client->id,
+            'CLIENT_NAME'        => $client->getFullName(),
             'CONSUMPTION_ID'     => (int)$data['consumptionId'],
             'CONSUMPTION_NUMBER' => (string)$data['consumptionNumber'],
             'AGREEMENT_NUMBER'   => (string)$data['agreementNumber'],
@@ -92,39 +158,65 @@ class TsReturnOperation extends ReferencesOperation
         }
 
         $emailFrom = getResellerEmailFrom($resellerId);
+
         // Получаем email сотрудников из настроек
-        $emails = getEmailsByPermit($resellerId, 'tsGoodsReturn');
-        if (!empty($emailFrom) && count($emails) > 0) {
-            foreach ($emails as $email) {
-                MessagesClient::sendMessage([
+        $emails = getEmailsByPermit($resellerId, TS_GOODS_RETURN);
+
+        /**
+         * @vphilippov:
+         * Классы MessagesClient и NotificationManager в коде не представлены,
+         * поэтому исходим из того, что вызовы их методов не содержат ошибок.
+         */
+
+        foreach ($emails as $email) {
+            MessagesClient::sendMessage([
                     0 => [ // MessageTypes::EMAIL
                            'emailFrom' => $emailFrom,
                            'emailTo'   => $email,
                            'subject'   => __('complaintEmployeeEmailSubject', $templateData, $resellerId),
                            'message'   => __('complaintEmployeeEmailBody', $templateData, $resellerId),
                     ],
-                ], $resellerId, NotificationEvents::CHANGE_RETURN_STATUS);
-                $result['notificationEmployeeByEmail'] = true;
+                ],
+                $resellerId,
+                $notificationType === self::TYPE_NEW
+                    ? NotificationEvents::NEW_RETURN_STATUS
+                    : NotificationEvents::CHANGE_RETURN_STATUS
+            );
 
-            }
+            $result['notificationEmployeeByEmail'] = true;
         }
 
         // Шлём клиентское уведомление, только если произошла смена статуса
-        if ($notificationType === self::TYPE_CHANGE && !empty($data['differences']['to'])) {
+        if ($notificationType === self::TYPE_CHANGE && isset($dataDifferences['to'])) {
             if (!empty($emailFrom) && !empty($client->email)) {
                 MessagesClient::sendMessage([
-                    0 => [ // MessageTypes::EMAIL
-                           'emailFrom' => $emailFrom,
-                           'emailTo'   => $client->email,
-                           'subject'   => __('complaintClientEmailSubject', $templateData, $resellerId),
-                           'message'   => __('complaintClientEmailBody', $templateData, $resellerId),
+                        0 => [ // MessageTypes::EMAIL
+                               'emailFrom' => $emailFrom,
+                               'emailTo'   => $client->email,
+                               'subject'   => __('complaintClientEmailSubject', $templateData, $resellerId),
+                               'message'   => __('complaintClientEmailBody', $templateData, $resellerId),
+                        ],
                     ],
-                ], $resellerId, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to']);
+                    $resellerId,
+                    $client->id,
+                    NotificationEvents::CHANGE_RETURN_STATUS,
+                    $dataDifferences['to']
+                );
+
                 $result['notificationClientByEmail'] = true;
             }
 
             if (!empty($client->mobile)) {
-                $res = NotificationManager::send($resellerId, $client->id, NotificationEvents::CHANGE_RETURN_STATUS, (int)$data['differences']['to'], $templateData, $error);
+                $error = null;
+                $res = NotificationManager::send(
+                    $resellerId,
+                    $client->id,
+                    NotificationEvents::CHANGE_RETURN_STATUS,
+                    $dataDifferences['to'],
+                    $templateData,
+                    $error           // Видимо передача по ссылке
+                );
+
                 if ($res) {
                     $result['notificationClientBySms']['isSent'] = true;
                 }
--- php/v2/others.php
+++ php/v2/others.php
@@ -2,19 +2,36 @@
 
 namespace NW\WebService\References\Operations\Notification;
 
+const TS_GOODS_RETURN = 'tsGoodsReturn';
+
 /**
+ * @vphilippov:
+ * Не понятно, почему свойство не объявлено в классе.
+ * Кроме этого странно, что в базовом классе содержатся объекты дочерних классов.
+ *
  * @property Seller $Seller
  */
 class Contractor
 {
     const TYPE_CUSTOMER = 0;
-    public $id;
-    public $type;
-    public $name;
 
-    public static function getById(int $resellerId): self
+    public int $id;
+    public int $type = self::TYPE_CUSTOMER;
+    public string $name = '';
+    public ?string $mobile = null;
+
+    public function __construct(int $id)
     {
-        return new self($resellerId); // fakes the getById method
+        $this->id = $id;
+    }
+
+    /**
+     * @param int $resellerId
+     * @return static|null
+     */
+    public static function getById(int $resellerId): ?static
+    {
+        return $resellerId ? new static($resellerId) : null; // fakes the getById method
     }
 
     public function getFullName(): string
@@ -27,23 +44,45 @@ class Seller extends Contractor
 {
 }
 
+/**
+ * @vphilippov:
+ * Странно, что класс Employee ("сотрудник") наследуется от Contractor ("подрядчик").
+ * Логически "сотрудник" не является "подрядчиком". Что-то не так в именовании или логике классов.
+ */
 class Employee extends Contractor
 {
 }
 
 class Status
 {
-    public $id, $name;
+    /**
+     * @vphilippov:
+     * Класс Status везде использутся только статически, нам точно нужны свойства id, name?
+     */
+    public int $id;
+    public string $name;
 
-    public static function getName(int $id): string
+    const COMPLETED = 'Completed';
+    const PENDING   = 'Pending';
+    const REJECTED  = 'Rejected';
+
+    public static function getName(int $id): ?string
     {
-        $a = [
-            0 => 'Completed',
-            1 => 'Pending',
-            2 => 'Rejected',
-        ];
+        return static::all()[$id] ?? null;
+    }
 
-        return $a[$id];
+    public static function checkId(int $id): bool
+    {
+        return !is_null(static::all()[$id] ?? null);
+    }
+
+    public static function all(): array
+    {
+        return [
+            self::COMPLETED,
+            self::PENDING,
+            self::REJECTED
+        ];
     }
 }
 
@@ -51,18 +90,18 @@ abstract class ReferencesOperation
 {
     abstract public function doOperation(): array;
 
-    public function getRequest($pName)
+    public function getRequest($pName): mixed
     {
-        return $_REQUEST[$pName];
+        return $_REQUEST[$pName] ?? null;
     }
 }
 
-function getResellerEmailFrom()
+function getResellerEmailFrom(): string
 {
     return 'contractor@example.com';
 }
 
-function getEmailsByPermit($resellerId, $event)
+function getEmailsByPermit(int $resellerId, string $event): array
 {
     // fakes the method
     return ['someemeil@example.com', 'someemeil2@example.com'];
