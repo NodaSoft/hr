daad0ce9deffc363fa8e03858c702be96c37b336
Anton Ardzhanov <ardzhanov.anton@wb.ru>
2023-12-07 22:52:13 +0300

    fix code for vacancy

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -15,90 +17,115 @@ import (
 // В конце должно выводить успешные таски и ошибки выполнены остальных тасков
 
 // A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+
+const (
+	mainChanSize = 10
+	execTime     = 3
+)
+
+type tTask struct {
+	id                int
+	createTime        time.Time // время создания
+	finishTime        time.Time // время выполнения
+	taskResult        string
+	createTimeInvalid bool
+}
+
+type chanExch struct {
+	inCh      chan tTask
+	successCh chan tTask
+	errorCh   chan tTask
+	eTask     []tTask
+	sTask     []tTask
+}
+
+func (ce *chanExch) CreateTasks(ctx context.Context) {
+
+	for {
+
+		select {
+		case <-ctx.Done():
+			close(ce.inCh)
+			return
+
+		default:
+			//fmt.Println(time.Now())
+			//time.Sleep(time.Second)
+			ce.inCh <- tTask{
+				id:                int(time.Now().Unix()),
+				createTime:        time.Now(),
+				taskResult:        "",
+				createTimeInvalid: time.Now().Nanosecond()%2 > 0,
+			}
+		}
+	}
+}
+
+func (ce *chanExch) workTasks() {
+	defer close(ce.successCh)
+	defer close(ce.errorCh)
+	for msg := range ce.inCh {
+
+		msg.taskResult = "something went wrong"
+		resultChan := ce.errorCh
+		// проверка условия msg.createTime.After(time.Now().Add(-20*time.Second)) убрана, т.к. по условию программа выполняется всего 3 секунды,
+		// сообщений в канале старше 20 секунд появится не может в принципе при данных условиях, всегда возвращает true
+		if msg.createTimeInvalid == false {
+			msg.taskResult = "task has been successed"
+			resultChan = ce.successCh
+
+		}
+		msg.finishTime = time.Now()
+		time.Sleep(time.Millisecond * 150)
+		resultChan <- msg
+
+	}
+}
+
+func (ce *chanExch) successTasks(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for msg := range ce.successCh {
+		ce.sTask = append(ce.sTask, msg)
+	}
+}
+
+func (ce *chanExch) errorTasks(wg *sync.WaitGroup) {
+	defer wg.Done()
+	for msg := range ce.errorCh {
+		ce.eTask = append(ce.eTask, msg)
+	}
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
+
+	channels := chanExch{
+		inCh:      make(chan tTask, mainChanSize),
+		successCh: make(chan tTask),
+		errorCh:   make(chan tTask),
 	}
 
-	superChan := make(chan Ttype, 10)
+	ctx, cancel := context.WithTimeout(context.Background(), execTime*time.Second)
+	defer cancel()
 
-	go taskCreturer(superChan)
+	go channels.CreateTasks(ctx)
+	go channels.workTasks()
 
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go channels.successTasks(&wg)
+	wg.Add(1)
+	go channels.errorTasks(&wg)
+	wg.Wait()
 
-		time.Sleep(time.Millisecond * 150)
-
-		return a
+	fmt.Println("Success task:")
+	for _, val := range channels.sTask {
+		fmt.Println(val.id, val.createTime.String(), val.finishTime.String(), val.taskResult, val.createTimeInvalid)
 	}
 
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
+	fmt.Println("Error task:")
+	for _, val := range channels.eTask {
+		fmt.Println(val.id, val.createTime.String(), val.finishTime.String(), val.taskResult, val.createTimeInvalid)
 	}
 
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
-
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
-
-	time.Sleep(time.Second * 3)
-
-	println("Errors:")
-	for r := range err {
-		println(r)
-	}
-
-	println("Done tasks:")
-	for r := range result {
-		println(r)
-	}
+	return
 }
