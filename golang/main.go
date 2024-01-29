94d7a41cb73dd2dfbf76ffde15af8bc9770bd4ba
Sergeev Daniil Albertovich <cnejniyray@gmail.com>
2024-01-29 15:31:15 +0300

    refactor golang

--- golang/main.go
+++ golang/main.go
@@ -1,104 +1,182 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
 	"time"
 )
 
-// ЗАДАНИЕ:
-// * сделать из плохого кода хороший;
-// * важно сохранить логику появления ошибочных тасков;
-// * сделать правильную мультипоточность обработки заданий.
-// Обновленный код отправить через merge-request.
-
-// приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме
-// В конце должно выводить успешные таски и ошибки выполнены остальных тасков
-
-// A Ttype represents a meaninglessness of our life
 type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+	Id         int64
+	CreatedAt  time.Time // время создания
+	FinishedAt time.Time // время выполнения
+	TaskResult string
+	Error      error
 }
 
-func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
+const (
+	TASK_RESULT_SUCCESS   = "task has been successed"
+	TASK_RESULT_UNSUCCESS = "something went wrong"
+	CHAN_SIZE             = 10
+)
+
+type TaskWorker struct {
+	mu *sync.Mutex
+	wg *sync.WaitGroup
+
+	newTasks    chan Ttype
+	doneTasks   chan Ttype
+	undoneTasks chan Ttype
+
+	completeTasks map[int64]Ttype
+	errorTasks    map[int64]Ttype
+}
+
+func NewTaskWorker() *TaskWorker {
+	return &TaskWorker{
+		mu:          &sync.Mutex{},
+		wg:          &sync.WaitGroup{},
+		doneTasks:   make(chan Ttype),
+		undoneTasks: make(chan Ttype),
+		newTasks:    make(chan Ttype, CHAN_SIZE),
+	}
+}
+
+func (tw *TaskWorker) taskCreturer(ctx context.Context) {
+	go func() {
+		for {
+			select {
+			default:
+				tw.wg.Add(1)
+
+				var err error
+				createdAt := time.Now()
+
+				// вместо time.Now().Nanoseconds()
+				id := createRandomId()
+
+				if id%2 > 0 { // вот такое условие появления ошибочных тасков
+					err = fmt.Errorf("Some error occured")
 				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+				tw.newTasks <- Ttype{CreatedAt: createdAt, Id: int64(id), Error: err} // передаем таск на выполнение
+
+				// охладить пыл по созданию тасок
+				time.Sleep(time.Millisecond * 50)
+
+			case <-ctx.Done():
+				return
+			}
+
+		}
+	}()
+}
+
+// time.Now().Nanosecond() всегда генерировал числа которые делятся на 2 без остатка, поэтому решил немного их рандомить
+func createRandomId() int {
+	rand.NewSource(time.Now().UnixNano())
+	min := 10
+	max := 30
+	r := rand.Intn(max-min+1) + min
+
+	return time.Now().Nanosecond() + r
+}
+
+func (tw *TaskWorker) taskHandler(task Ttype) {
+	if task.Error == nil && task.CreatedAt.After(time.Now().Add(-20*time.Second)) {
+		task.TaskResult = TASK_RESULT_SUCCESS
+		task.FinishedAt = time.Now()
+		tw.doneTasks <- task
+	} else {
+		task.TaskResult = TASK_RESULT_UNSUCCESS
+		task.FinishedAt = time.Now()
+		log.Printf("Task id %d time %s, error %s", task.Id, task.CreatedAt, task.TaskResult)
+		tw.undoneTasks <- task
+	}
+
+}
+
+func (tw *TaskWorker) createTaskHandlers(handlerCount int64) {
+	for i := 0; i < int(handlerCount); i++ {
+		go func() {
+			// получение тасков
+			for t := range tw.newTasks {
+				task := t
+				go tw.taskHandler(task)
 			}
 		}()
 	}
+}
 
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
+func (tw *TaskWorker) taskOutput() {
 	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
+		for r := range tw.doneTasks {
+			tw.mu.Lock()
+			tw.completeTasks[r.Id] = r
+			tw.mu.Unlock()
+
+			tw.wg.Done()
 		}
-		close(superChan)
+
 	}()
 
-	result := map[int]Ttype{}
-	err := []error{}
 	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
+		for r := range tw.undoneTasks {
+			tw.mu.Lock()
+			tw.errorTasks[r.Id] = r
+			tw.mu.Unlock()
+
+			tw.wg.Done()
 		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
+
+	}()
+}
+
+func (tw *TaskWorker) Start(
+	ctxTimeout context.Context,
+	handlerCount int64,
+) {
+	ctx, stop := signal.NotifyContext(ctxTimeout, os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	// запускаем процесс создания тасок
+	tw.taskCreturer(ctx)
+
+	// создаем n обработчиков тасок
+	tw.createTaskHandlers(handlerCount)
+
+	// запускаем процесс записи тасок в мапы
+	tw.taskOutput()
+
+	// подобие graceful shutdown
+	func() {
+		<-ctx.Done()
+		fmt.Println("APP CLOSING")
+		close(tw.newTasks)
+		tw.wg.Wait()
 	}()
 
-	time.Sleep(time.Second * 3)
-
-	println("Errors:")
-	for r := range err {
+	println("Error tasks:")
+	for r := range tw.errorTasks {
 		println(r)
 	}
 
 	println("Done tasks:")
-	for r := range result {
+	for r := range tw.completeTasks {
 		println(r)
 	}
+}
+
+func main() {
+	// Таймаут через которое приложение закроется
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	tw := NewTaskWorker()
+	tw.Start(ctx, CHAN_SIZE)
 }
