c3e8aadbaec0ff706cbc3b6d002d92b2eba56279
yanik20006 <yanik20006@gmail.com>
2024-04-11 20:54:23 +0300

    fixed initial code

--- golang/main.go
+++ golang/main.go
@@ -5,101 +5,64 @@ import (
 	"time"
 )
 
-// ЗАДАНИЕ:
-// * сделать из плохого кода хороший;
-// * важно сохранить логику появления ошибочных тасков;
-// * сделать правильную мультипоточность обработки заданий.
-// * обновленный код отправить через pull-request.
+const (
+	maxConcurrentTasks = 10
+	workTime           = 30
+)
 
-// Приложение эмулирует получение и обработку тасков, пытается и получать и обрабатывать в многопоточном режиме.
-// Должно выводить успешные таски и ошибки по мере выполнения.
-// Как видите, никаких привязок к внешним сервисам нет - полный карт-бланш на модификацию кода.
-
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+type Task struct {
+	id        int
+	createdAt time.Time
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
+	startTime := time.Now()
+
+	taskQueue := make(chan Task)
+
+	go taskCreator(taskQueue, startTime)
+
+	workerDone := make(chan bool)
+
+	for i := 0; i < maxConcurrentTasks; i++ {
+		go taskWorker(taskQueue, startTime, workerDone)
 	}
 
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
+	for i := 0; i < maxConcurrentTasks; i++ {
+		<-workerDone
 	}
 
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
+	fmt.Println("Done.")
+}
 
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+func taskCreator(c chan Task, startTime time.Time) {
+	for time.Since(startTime).Seconds() < workTime {
+		createdAt := time.Now()
+		id := int(time.Now().Unix())
+		task := Task{id: id, createdAt: createdAt}
+
+		if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+			fmt.Printf("Task %d failed at creation time %s\n", task.id, task.createdAt)
 		}
+
+		c <- task
 	}
 
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
+	close(c)
+}
 
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
+func taskRunner(t Task, startTime time.Time) {
+	if t.createdAt.Before(startTime.Add(20 * time.Second)) {
+		fmt.Printf("Task %d (created at %s) is done at %s\n", t.id, t.createdAt, time.Now())
+	} else {
+		fmt.Printf("Task %d (created at %s) failed\n", t.id, t.createdAt)
+	}
+}
 
-	time.Sleep(time.Second * 3)
-
-	println("Errors:")
-	for r := range err {
-		println(r)
+func taskWorker(taskQueue chan Task, startTime time.Time, workerDone chan bool) {
+	for task := range taskQueue {
+		taskRunner(task, startTime)
 	}
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
-	}
+	workerDone <- true
 }
