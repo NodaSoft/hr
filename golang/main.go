a8b8c7c947b65edab6e8dbe2b7a5a7acb545a46a
avealice <minisol@bk.ru>
2024-04-22 12:48:36 +0300

    refactor

--- golang/main.go
+++ golang/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -13,91 +15,116 @@ import (
 // Обновленный код отправить через pull-request в github
 // Как видите, никаких привязок к внешним сервисам нет - полный карт-бланш на модификацию кода.
 
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+type Task struct {
+	ID         int
+	CreateTime time.Time // время создания задачи
+	FinishTime time.Time // время завершения выполнения задачи
+	Result     string    // результат выполнения задачи
+	Err        error     // ошибка, если таковая возникла при выполнении задачи
+}
+
+// taskCreator генерирует задачи и отправляет их в канал taskChan.
+// когда приходит время таймаута завершаем создание тасок
+func taskCreator(ctx context.Context, taskChan chan<- Task) {
+	defer close(taskChan)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			// Создаем новую задачу с уникальным ID и текущим временем создания.
+			task := Task{
+				ID:         int(time.Now().UnixNano()),
+				CreateTime: time.Now(),
+			}
+
+			if task.CreateTime.Nanosecond()%2 != 0 {
+				task.Err = fmt.Errorf("Some error occurred")
+				task.Result = "something went wrong"
+			} else {
+				task.Result = "task has been successed"
+			}
+
+			taskChan <- task
+		}
+	}
+
+}
+
+// taskWorker обрабатывает задачи из канала taskChan и отправляет результаты в канал doneTaskChan.
+func taskWorker(task Task, doneTaskChan chan<- Task, errChan chan<- error) {
+	// Эмулируем выполнение задачи и устанавливаем время завершения.
+	if task.CreateTime.After(time.Now().Add(-20 * time.Second)) {
+		task.Result = "task has been successed"
+	} else {
+		task.Result = "something went wrong"
+		task.Err = fmt.Errorf("something went wrong")
+	}
+	task.FinishTime = time.Now()
+
+	// Эмулируем время обработки задачи.
+	time.Sleep(time.Millisecond * 150)
+
+	doneTaskChan <- task
+	if task.Err != nil {
+		errChan <- fmt.Errorf("Task id %d time %s, error %s", task.ID, task.CreateTime, task.Result)
+	}
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
+	// Создаем контекст с таймаутом 10 секунд.
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	// Создаем каналы для передачи задач, завершенных задач и ошибок.
+	taskChan := make(chan Task, 10)
+	doneTaskChan := make(chan Task, 10)
+	errChan := make(chan error, 10)
+
+	var wg sync.WaitGroup
+
+	// Запускаем горутину для генерации задач.
+	go taskCreator(ctx, taskChan)
+
+	// Запускаем несколько горутин для обработки задач.
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
 		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+			defer wg.Done()
+			for t := range taskChan {
+				taskWorker(t, doneTaskChan, errChan)
 			}
 		}()
 	}
 
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
+	// Ожидаем завершения всех горутин обработки задач.
 	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
+		wg.Wait()
+		close(doneTaskChan)
+		close(errChan)
 	}()
 
-	result := map[int]Ttype{}
-	err := []error{}
+	// Создаем WaitGroup для отслеживания завершения вывода результатов.
+	var wgPrint sync.WaitGroup
+	wgPrint.Add(1)
 	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
+		defer wgPrint.Done()
+		for {
+			select {
+			case r, ok := <-doneTaskChan:
+				if !ok {
+					return
+				}
+				fmt.Printf("ID: %d, Create Time: %s, Finish Time: %s, Result: %s\n", r.ID, r.CreateTime.Format("2006-01-02 15:04:05"), r.FinishTime.Format("2006-01-02 15:04:05"), r.Result)
+			case err, ok := <-errChan:
+				if !ok {
+					return
+				}
+				fmt.Println("Error:", err)
+			}
 		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
 	}()
 
-	time.Sleep(time.Second * 3)
+	wgPrint.Wait()
 
-	println("Errors:")
-	for r := range err {
-		println(r)
-	}
-
-	println("Done tasks:")
-	for r := range result {
-		println(r)
-	}
 }
