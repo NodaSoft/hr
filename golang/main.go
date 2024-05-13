f6823f621aa3786161b315f5c715e25f6ee64fd7
Vova4o <gavrilenko2012@gmail.com>
2024-05-13 16:13:36 +0300

    did requested changes to go file

--- golang/main.go
+++ golang/main.go
@@ -11,6 +11,8 @@ import (
 // ЗАДАНИЕ: сделать из плохого кода хороший и рабочий - as best as you can.
 // Важно сохранить логику появления ошибочных тасков.
 // Важно оставить асинхронные генерацию и обработку тасков.
+// Совсем не понятно почму не синхронизировать? Вероятность одновременной записи очень большая
+// Это может привести к потере данных или не правильной записи...
 // Сделать правильную мультипоточность обработки заданий.
 // Обновленный код отправить через pull-request в github
 // Как видите, никаких привязок к внешним сервисам нет - полный карт-бланш на модификацию кода.
@@ -23,83 +25,79 @@ type Ttype struct {
 	taskRESULT []byte
 }
 
-func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+func taskCreturer(a chan Ttype) {
+	go func() {
+		for {
+			ft := time.Now().Format(time.RFC3339)
+			if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+				ft = "Some error occured"
 			}
-		}()
-	}
-
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
+			a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
 		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
+	}()
+}
 
-		time.Sleep(time.Millisecond * 150)
-
-		return a
+func taskWorker(a Ttype) Ttype {
+	tt, _ := time.Parse(time.RFC3339, a.cT)
+	if tt.After(time.Now().Add(-20 * time.Second)) {
+		a.taskRESULT = []byte("task has been successed")
+	} else {
+		a.taskRESULT = []byte("something went wrong")
 	}
+	a.fT = time.Now().Format(time.RFC3339Nano)
 
+	time.Sleep(time.Millisecond * 150)
+
+	return a
+}
+
+func taskSorter(t Ttype, doneTasks chan Ttype, undoneTasks chan error) {
+	if string(t.taskRESULT[14:]) == "successed" {
+		doneTasks <- t
+	} else {
+		undoneTasks <- fmt.Errorf("task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+	}
+}
+
+func taskReceiver(superChan chan Ttype, doneTasks chan Ttype, undoneTasks chan error) {
+	for t := range superChan {
+		t = taskWorker(t)
+		go taskSorter(t, doneTasks, undoneTasks)
+	}
+	close(superChan)
+}
+
+func taskResultHandler(doneTasks chan Ttype, undoneTasks chan error, result map[int]Ttype, err *[]error) {
+	for r := range doneTasks {
+		result[r.id] = r
+	}
+	for r := range undoneTasks {
+		*err = append(*err, r)
+	}
+	close(doneTasks)
+	close(undoneTasks)
+}
+
+func main() {
+	superChan := make(chan Ttype, 10)
 	doneTasks := make(chan Ttype)
 	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
-
 	result := map[int]Ttype{}
 	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
+
+	taskCreturer(superChan)
+	go taskReceiver(superChan, doneTasks, undoneTasks)
+	go taskResultHandler(doneTasks, undoneTasks, result, &err)
 
 	time.Sleep(time.Second * 3)
 
 	println("Errors:")
-	for r := range err {
-		println(r)
+	for _, e := range err {
+		println(e.Error())
 	}
 
 	println("Done tasks:")
-	for r := range result {
-		println(r)
+	for _, r := range result {
+		println(r.id)
 	}
 }
