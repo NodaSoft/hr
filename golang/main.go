fc82539ef1db2164d1b1c45eaa1e785354b435f5
Jetta Jacks <jettajac@su-d3.msk.21-school.ru>
2024-06-05 14:29:06 +0300

    done

--- golang/main.go
+++ golang/main.go
@@ -16,150 +16,158 @@ import (
 // Обновленный код отправить через pull-request в github
 // Как видите, никаких привязок к внешним сервисам нет - полный карт-бланш на модификацию кода.
 
-// !!! Меняем на более читаемый нейминг
-// !!!Вынесла функции из мейл
+// Резюме проделанной работы:
+// - Изменила на более понятный нейминг
+// - Вывела логику всех функций из майл
+// - Добавила глобальный переменные, чтоб было проше настраивать и тестить программу ()
+
+// Не претендую, что код подвергся достаточному рефакторингу, исходила из желания,
+// чтоб он ста более читаем, действительно выводил каждые 3 секунды результаты проделанных работ, чтоб не было гонки и прочих проблем горутин
 
 // A Task represents a meaninglessness of our life
 type Task struct {
 	id         int
 	createTime string // время создания
 	finishTime string // время выполнения
-	taskResult string // !!! возможно сделать стринг
+	taskResult string // результат выполнения таска
 }
 
-var tmpTime time.Duration = 4
+type ResultTasks struct {
+	DoneTask  []Task // Канал для выполненных тасков
+	ErrorTask []Task // Канал для тасков, которые отработали неккоректно
+}
 
-func taskGenerate(superChan chan Task, wg *sync.WaitGroup) { // !!!функция только на отдавания в канал// !!! а переименновать
-	//сделать тайминг работы 10
+var (
+	generateTime        time.Duration = 10 // Время, в течение которого генерируются таски, секунды
+	outputTime          time.Duration = 3  // Частота появления  вывода результатов работы программы, секунды
+	generationfrequency time.Duration = 1  // Время, требуемое для генерации одного таска, милисекунды
+)
+
+// tickerResult - функция отвечает за вывод в консоль результатов работы программы каждые 3 секунды
+// в исходном коде функционал вывода каждые 3 секунды был реализован некорректно
+func tickerResult(mu *sync.Mutex, resultTasks *ResultTasks, resultChan chan ResultTasks, stop chan bool) {
+	ticker := time.NewTicker(outputTime * time.Second)
+	go func() { // запускаем в горутине функцию, которая отлавливает завершения работы обработчика тасков
+		_, ok := <-stop
+		if ok {
+			mu.Lock()
+			resultChan <- *resultTasks
+			close(resultChan)
+			return
+		}
+	}()
+	for range ticker.C { // срабатывает каждые 3 секунды
+		mu.Lock()
+		resultChan <- *resultTasks
+		mu.Unlock()
+	}
+}
+
+// taskGenerate - функция отвечает за генерацию тасков в течение заявленного времени, 10 секунд.
+// В исходном коде код генерируеться бесконечно
+func taskGenerate(generateChan chan Task, wg *sync.WaitGroup) {
+	defer wg.Done()
+	wg.Add(1)
+
 	start := time.Now()
 	for {
-
-		if time.Since(start) >= tmpTime*time.Second {
-
-			close(superChan) // Закрываем канал после 10 секунд
-			fmt.Println("Generate - stop")
-			return // Выходим из функции
-		} // !!!
-		fmt.Println("_____1")
-
-		generateTime := time.Now().Format(time.RFC3339)
-		if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-			generateTime = "Some error occured"
+		if time.Since(start) >= generateTime*time.Second {
+			close(generateChan) // Закрываем канал после 10 секунд
+			return              // Выходим из функции
+		}
+		createTime := time.Now().Format(time.RFC3339)
+		// При Nanosecond негативные таски не генерируются, но если изменить на секунды/миллисекунды  все ок, оригинал также,
+		//по условию задания не меняем это
+		if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков !!!
+			createTime = "Some error occured"
 		}
 		wg.Add(1)
-		superChan <- Task{createTime: generateTime, id: int(time.Now().Unix())} // передаем таск на выполнение
-		time.Sleep(1 * time.Second)                                             // Ограничение на 1 секунду
-
+		generateChan <- Task{createTime: createTime, id: int(time.Now().Unix())} // передаем таск на выполнение
+		time.Sleep(generationfrequency * time.Millisecond)                       // Ограничениеб если хотим другую периодичность появления тасков
 	}
-	// !!! возможно здесь надо сделать слип 10 секунд, и счетчик горутин
 }
 
-func taskWorker(a Task, doneTasks chan Task) Task { /// naming
-	tt, _ := time.Parse(time.RFC3339, a.createTime)
-	if tt.After(time.Now().Add(-2 * time.Second)) {
-		a.taskResult = "task has been successed"
+// processingTasks  - функция отвечает за чтение из generateChan и запуска непосредственно функции выполнение тасков
+// Вывела отделно для простотв чтения кода и посика ошибок
+func processingTasks(generateChan, doneChan, errorChan chan Task) {
+	wg_t := sync.WaitGroup{} // сделала внутри функции waitGroup, чтоб должаться окончания выполнения всех таска
+	for task_c := range generateChan {
+		wg_t.Add(1)
+		go taskWorker(task_c, doneChan, errorChan, &wg_t)
+	}
+	wg_t.Wait()
+	close(doneChan)
+	close(errorChan)
+}
 
+// taskWorker - функция отвечает за выполнение таска
+func taskWorker(task Task, doneChan, errorChan chan Task, wg *sync.WaitGroup) {
+	defer wg.Done()
+	tt, _ := time.Parse(time.RFC3339, task.createTime)
+	if tt.After(time.Now().Add(-20 * time.Second)) {
+		task.taskResult = "task has been successed"
+		task.finishTime = time.Now().Format(time.RFC3339Nano)
+		doneChan <- task
 	} else {
-		a.taskResult = "something went wrong"
+		task.taskResult = "something went wrong"
+		task.finishTime = time.Now().Format(time.RFC3339Nano)
+		errorChan <- task
 	}
-	a.finishTime = time.Now().Format(time.RFC3339Nano)
-
-	time.Sleep(time.Millisecond * 150) //!!! не очень понятно зачем эта задержка, функциональной нагрузке не несет
-
-	doneTasks <- a
-	fmt.Println("G")
-	return a
+	time.Sleep(time.Millisecond * 150)
 }
 
+// storage - функция отвечает за хранение отработанных тасков
+func storage(current chan Task, storage *[]Task, wg *sync.WaitGroup, mu *sync.Mutex, stop chan bool) {
+	defer func() { stop <- true }() // отлавливаем завершения работы функции
+	defer wg.Done()
+
+	wg.Add(1)
+	for task := range current {
+		mu.Lock()
+		*storage = append(*storage, task)
+		mu.Unlock()
+		wg.Done()
+	}
+}
+
+// printResult - функция отвечает за вывод результатов работы программы
+func printResult(resultChan chan ResultTasks, wg *sync.WaitGroup) {
+	defer wg.Done()
+	wg.Add(1)
+	for result := range resultChan {
+		fmt.Println("Done tasks:")
+		for _, task_d := range result.DoneTask {
+			fmt.Println(task_d)
+		}
+		fmt.Println("Errors:")
+		for _, task_e := range result.ErrorTask {
+			fmt.Printf("Task id %d time %s, error %s\n", task_e.id, task_e.createTime, task_e.taskResult)
+		}
+	}
+}
+
+// main - функция запускает программу
 func main() {
 	var wg sync.WaitGroup
-	superChan := make(chan Task, 10)
-	fmt.Println("Start")
-	// Генерируем таски
-	go taskGenerate(superChan, &wg)
+	generateChan := make(chan Task, 10) // !!1 переименновать канал
+	doneChan := make(chan Task, 10)
+	errorChan := make(chan Task, 10)
+	resultChan := make(chan ResultTasks)
+	stop := make(chan bool)
 
-	// workerTasks := make(chan Task)
-	doneTasks := make(chan Task)
-	// resultsChan := make(chan Task)
-	// undoneTasks := make(chan error)
-	ticker := time.NewTicker(1 * time.Second)
+	result := ResultTasks{DoneTask: []Task{}, ErrorTask: []Task{}}
 	mu := sync.Mutex{}
-	// println("ttttt____________")
-	result := map[int]Task{}
-	go func() {
-		// println("sssss____________")
-		for range ticker.C {
-			println("Errors:")
-			// for r := range err {
-			// 	println(r)
-			// 	println("error_t", r)
-			// }
+	// Отсеживаем время выводы в терминал результатов
+	go tickerResult(&mu, &result, resultChan, stop)
+	// Генерируем таски
+	go taskGenerate(generateChan, &wg)
+	// Запускаем таски
+	go processingTasks(generateChan, doneChan, errorChan)
+	// Храним все отработанные таски
+	go storage(doneChan, &result.DoneTask, &wg, &mu, stop)
+	go storage(errorChan, &result.ErrorTask, &wg, &mu, stop)
+	// Распечатываем все отработанные таски н текущий момент
+	printResult(resultChan, &wg)
 
-			println("Done tasks:")
-			// for r := range doneResults {
-			mu.Lock()
-			for task_r := range result {
-				// _ = task_r
-				fmt.Println(task_r)
-			}
-
-			// fmt.Println("____1")
-			mu.Unlock()
-			// }
-			// fmt.Println("____")
-			// wg.Wait()
-		}
-
-	}()
-
-	go func() {
-		for task_c := range superChan {
-
-			// _ = task
-
-			// _ = task
-			fmt.Println(5)
-			task_c = taskWorker(task_c, doneTasks) //!!!каналы сделать однонаправленные
-
-		}
-	}()
-
-	go func() {
-		defer wg.Done()
-		// fmt.Println("____2/1")
-		for r := range doneTasks {
-			fmt.Println("____2")
-			// go func(r Task) {
-			mu.Lock()
-			result[r.id] = r
-			mu.Unlock()
-
-			// }(r)
-		}
-		// close(doneTasks)
-	}()
-	// fmt.Println("____5")
-	go func() {
-		for {
-			// 	// fmt.Println("____7")
-			// 	// _, ok := <-doneTasks
-			// 	// if !ok {
-			fmt.Println("____3")
-			wg.Wait()
-			return
-			// fmt.Println("____6")
-			// 	return
-			// 	// канал закрыт
-		}
-
-		// 	// 	// close(doneTasks)
-		// 	// 	// close(undoneTasks)
-	}()
-
-	// }
-
-	// }()
-	// time.Sleep(3 * time.Second)
-	// wg.Wait()
-	fmt.Println("Все горутины завершены")
+	wg.Wait()
 }
