f37185c8f87483bf1e0efb8dbce5313b07d2eb75
mgodunov <mikhailgodunow@gmail.com>
2024-06-18 00:54:10 +0300

    fixing send on closed channel bug

--- golang/internal/creator/creator.go
+++ golang/internal/creator/creator.go
@@ -9,18 +9,23 @@ import (
 type Creator struct{}
 
 func (c *Creator) Create(ctx context.Context, tasks chan<- task.Task) {
-	go func() {
-		go func() {
-			for {
-				creationTime := time.Now().Format(time.RFC3339)
-				if time.Now().UnixMilli()%2 > 0 { // при .Nanosecond() получаем всегда 2 нуля -> ошибочных всегда 0
-					creationTime = "Some error occured"
-				}
-				tasks <- task.Task{CreationTime: creationTime,
-					Id: int(time.Now().UnixNano())} // больше не генерируем таски с одинаковыми ID
-			}
-		}()
-		<-ctx.Done()
-		close(tasks)
-	}()
+    go func(ctx context.Context) {
+        defer close(tasks)
+        for {
+            select {
+            case <-ctx.Done():
+                return
+            default:
+                creationTime := time.Now().Format(time.RFC3339)
+                if time.Now().UnixMilli()%2 > 0 {  // при .Nanosecond() получаем всегда 2 нуля ->
+                    creationTime = "Some error occurred"
+                }
+                select {
+                case tasks <- task.Task{CreationTime: creationTime, Id: int(time.Now().UnixNano())}: //// больше не генерируем таски с одинаковыми ID
+                case <-ctx.Done():
+                    return
+                }
+            }
+        }
+    }(ctx)
 }
