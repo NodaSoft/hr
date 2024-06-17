1f7c060d7951ed37fa7d6e121d0949ecac258b75
mgodunov <mikhailgodunow@gmail.com>
2024-06-17 20:23:58 +0300

    rename ttype to task & add go.mod file

---
+++ golang/internal/domain/task/task.go
@@ -0,0 +1,9 @@
+package task
+
+// A Ttype represents a meaninglessness of our life
+type Task struct {
+	Id            int
+	CreationTime  string
+	Executiontime string
+	TaskResult    []byte
+}
--- golang/internal/domain/ttype/ttype.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id            int
-	creationTime  string
-	executiontime string
-	taskResult    []byte
-}
