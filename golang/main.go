da5a07088d2e4511f303b619ff0971852f154a8f
Heater <belckbird19@gmail.com>
2024-07-21 21:06:22 +0300

    golang fix

--- golang/main.go
+++ golang/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
@@ -23,15 +24,36 @@ type Ttype struct {
 	taskRESULT []byte
 }
 
+const (
+	workTimeSeconds  = 10
+	printTimeSeconds = 3
+)
+
 func main() {
+	// таймеры времени
+	workTime := time.After(time.Second * workTimeSeconds)
+	printTimer := time.NewTimer(time.Second * printTimeSeconds)
+
+	// канал нужен чтобы корректно завершать программу после истечения времени и закрытия результирующего канала
+	gracefulShutdownChan := make(chan struct{})
+
+	// ассинхронное создание тасок
 	taskCreturer := func(a chan Ttype) {
 		go func() {
 			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
+				select {
+				case <-workTime:
+					// по истечению времени закрываем канал и передаем сигнал о завершении работы программы
+					close(a)
+					gracefulShutdownChan <- struct{}{}
+					return
+				default:
+					ft := time.Now().Format(time.RFC3339)
+					if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+						ft = "Some error occured"
+					}
+					a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнени
 				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
 			}
 		}()
 	}
@@ -57,7 +79,9 @@ func main() {
 	doneTasks := make(chan Ttype)
 	undoneTasks := make(chan error)
 
-	tasksorter := func(t Ttype) {
+	tasksorter := func(wg *sync.WaitGroup, t Ttype) {
+		defer wg.Done()
+
 		if string(t.taskRESULT[14:]) == "successed" {
 			doneTasks <- t
 		} else {
@@ -66,40 +90,67 @@ func main() {
 	}
 
 	go func() {
+		wg := &sync.WaitGroup{}
+
 		// получение тасков
 		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
+			wg.Add(1)
+			go func(t Ttype) {
+				t = task_worker(t)
+				go tasksorter(wg, t)
+			}(t)
 		}
-		close(superChan)
-	}()
 
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
+		// ждём закрытия канала, генерирующего таски
+		// также ожидаем завершения работы горутин на запись в каналы результата и ошибок
+		wg.Wait()
 		close(doneTasks)
 		close(undoneTasks)
 	}()
 
-	time.Sleep(time.Second * 3)
+	result := map[int]Ttype{}
+	mux := &sync.Mutex{}
 
-	println("Errors:")
-	for r := range err {
-		println(r)
-	}
+	err := []error{}
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
+	// запись в буффер
+	go func() {
+
+		go func() {
+			for r := range doneTasks {
+				go func(r Ttype) {
+					mux.Lock()
+					result[r.id] = r
+					mux.Unlock()
+				}(r)
+			}
+		}()
+
+		go func() {
+			for r := range undoneTasks {
+				go func(r error) {
+					err = append(err, r)
+				}(r)
+			}
+		}()
+
+	}()
+
+	for {
+		select {
+		case <-gracefulShutdownChan:
+			return
+		case <-printTimer.C:
+			printTimer.Reset(time.Second * printTimeSeconds)
+			fmt.Println("Errors:")
+			for r := range err {
+				fmt.Println(r)
+			}
+
+			fmt.Println("Done tasks:")
+			for r := range result {
+				fmt.Println(r)
+			}
+		}
 	}
 }
