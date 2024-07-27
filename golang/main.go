7387d810bc3f4de24e12cad2f0b90570fae84335
andrew-nino <andrey.muravey1@gmail.com>
2024-07-27 16:14:13 +0300

    solution is as good as possible)

--- golang/main.go
+++ golang/main.go
@@ -1,105 +1,188 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"strings"
+	"sync"
 	"time"
 )
 
-// Приложение эмулирует получение и обработку неких тасков. Пытается и получать, и обрабатывать в многопоточном режиме.
-// Приложение должно генерировать таски 10 сек. Каждые 3 секунды должно выводить в консоль результат всех обработанных к этому моменту тасков (отдельно успешные и отдельно с ошибками).
+var (
+	execution_time int
+	frequency      int
+)
 
-// ЗАДАНИЕ: сделать из плохого кода хороший и рабочий - as best as you can.
-// Важно сохранить логику появления ошибочных тасков.
-// Важно оставить асинхронные генерацию и обработку тасков.
-// Сделать правильную мультипоточность обработки заданий.
-// Обновленный код отправить через pull-request в github
-// Как видите, никаких привязок к внешним сервисам нет - полный карт-бланш на модификацию кода.
-
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+func init() {
+	flag.IntVar(&execution_time, "time", 10, "Task execution time")
+	flag.IntVar(&frequency, "frequency", 150, "How often the task is created: 1 per frequency in milliseconds")
+	flag.Parse()
 }
 
+const (
+	not_successfully = "something went wrong"
+	successfully     = "task has been successed"
+)
+
+// A Tasker represents a meaninglessness of our life
+type Tasker struct {
+	id           int
+	created_at   string
+	completed_in string
+	task_broken  bool
+	description  string
+}
+
+// Closing function
+func intSeq() func() int {
+	i := 0
+	return func() int {
+		i++
+		return i
+	}
+}
+// The closure is used as a sequence number for identification.
+var nextID = intSeq()
+
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
-	}
 
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
+	created_tasks := make(chan Tasker)
+	doneTasks := make(chan Tasker)
 	undoneTasks := make(chan error)
 
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*time.Duration(execution_time))
+	defer cancel()
+
+	fmt.Println("Start processing tasks...")
+
+	go TaskCreturer(ctx, created_tasks)
+
+	go TaskProcessing(created_tasks, doneTasks, undoneTasks)
+
+	Result(doneTasks, undoneTasks)
+
+	fmt.Println("\nEnd of task processing...")
+}
+
+func TaskCreturer(ctx context.Context, created_tasks chan Tasker) {
+	for {
+		select {
+		case <-ctx.Done():
+			close(created_tasks)
+			return
+		default:
+			var broken bool
+			time_now := time.Now().Format(time.RFC3339)
+			if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+				broken = true
+			}
+
+			time.Sleep(time.Millisecond * time.Duration(frequency))
+			created_tasks <- Tasker{id: nextID(), created_at: time_now, task_broken: broken} // передаем таск на выполнение
 		}
 	}
+}
 
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
-	}()
+func TaskProcessing(created_tasks chan Tasker, doneTasks chan Tasker, undoneTasks chan error) {
+	processed_tasks := make(chan Tasker)
+	wg := sync.WaitGroup{}
 
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
+	for task := range created_tasks {
+		wg.Add(1)
+		Processing(task, processed_tasks)
+		Sorter(processed_tasks, doneTasks, undoneTasks, &wg)
+
+	}
+	defer func() {
+		wg.Wait()
+		close(processed_tasks)
 		close(doneTasks)
 		close(undoneTasks)
 	}()
+}
 
-	time.Sleep(time.Second * 3)
+func Processing(tsk Tasker, processed_tasks chan Tasker) {
 
-	println("Errors:")
-	for r := range err {
-		println(r)
-	}
+	go func() {
+		if tsk.task_broken {
+			tsk.description = not_successfully
+		} else {
+			tsk.description = successfully
+		}
+		tsk.completed_in = time.Now().Format(time.RFC3339Nano)
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
+		processed_tasks <- tsk
+	}()
+}
+
+func Sorter(processed_tasks chan Tasker, doneTasks chan Tasker, undoneTasks chan error, wg *sync.WaitGroup) {
+
+	go func() {
+		t := <-processed_tasks
+
+		if strings.HasSuffix(t.description, "successed") {
+			doneTasks <- t
+		} else {
+			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.created_at, t.description)
+		}
+		wg.Done()
+	}()
+}
+
+func Result(doneTasks chan Tasker, undoneTasks chan error) {
+
+	var processed = make(map[int]Tasker)
+	var unprocessed = make([]error, 0)
+
+	completion_done := make(chan struct{})
+	completion_undone := make(chan struct{})
+	completion_full := make(chan struct{})
+
+	go func() {
+		for r := range doneTasks {
+			processed[r.id] = r
+		}
+		completion_done <- struct{}{}
+		close(completion_done)
+	}()
+	go func() {
+		for r := range undoneTasks {
+			unprocessed = append(unprocessed, r)
+		}
+		completion_undone <- struct{}{}
+		close(completion_undone)
+	}()
+
+	go func() {
+		<-completion_done
+		<-completion_undone
+		completion_full <- struct{}{}
+	}()
+
+	var iteration int
+	for {
+		time.Sleep(time.Second * 3)
+		iteration++
+		fmt.Println("\nIteration:", iteration)
+		fmt.Println("*****************************************************************")
+
+		fmt.Println("Done tasks:")
+		for _, p := range processed {
+			fmt.Println(p)
+		}
+		clear(processed)
+
+		fmt.Println("Errors:")
+		for _, e := range unprocessed {
+			fmt.Println(e)
+		}
+		unprocessed = unprocessed[:0]
+
+		select {
+		case <-completion_full:
+			return
+		default:
+		}
 	}
 }
