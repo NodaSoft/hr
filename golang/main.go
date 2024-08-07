56cb0dbb0360305638c64cfc006386294305b9d5
Ser9unin9 <rockerror@gmail.com>
2024-08-07 18:39:19 +0300

    Denis Sergunin done this main.go

--- golang/main.go
+++ golang/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"sync"
 	"time"
 )
 
 // Приложение эмулирует получение и обработку неких тасков. Пытается и получать, и обрабатывать в многопоточном режиме.
-// Приложение должно генерировать таски 10 сек. Каждые 3 секунды должно выводить в консоль результат всех обработанных к этому моменту тасков (отдельно успешные и отдельно с ошибками).
+// Приложение должно генерировать таски 10 сек.
+// Каждые 3 секунды должно выводить в консоль результат всех обработанных к этому моменту тасков (отдельно успешные и отдельно с ошибками).
 
 // ЗАДАНИЕ: сделать из плохого кода хороший и рабочий - as best as you can.
 // Важно сохранить логику появления ошибочных тасков.
@@ -20,86 +23,124 @@ type Ttype struct {
 	id         int
 	cT         string // время создания
 	fT         string // время выполнения
-	taskRESULT []byte
+	taskRESULT string
+}
+
+func taskWorker(a Ttype) Ttype {
+	_, err := time.Parse(time.RFC3339, a.cT)
+	// если не менять в taskCreator логику создания ошибки,
+	// то time.Parse не сможет парсить a.cT и вернет ошибку есть смысл по ней и выходить как мне кажется
+	if err != nil {
+		a.taskRESULT = "something went wrong"
+		log.Println("Parsing Error")
+		a.fT = time.Now().Format(time.RFC3339Nano)
+		return a
+	}
+	a.taskRESULT = "task has been successed"
+	a.fT = time.Now().Format(time.RFC3339Nano)
+	time.Sleep(time.Millisecond * 151)
+	return a
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
+	var wg sync.WaitGroup
+	superChan := make(chan Ttype, 10)
+	doneCh := make(chan struct{})
+
+	// генерируем задачи 10 секунд
+	ticker := time.NewTicker(10 * time.Second)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		defer close(superChan)
+		// останавливаемся генерацию по тикеру
+		for {
+			select {
+			case <-ticker.C:
+				log.Println("Кончилось время генерации")
+				ticker.Stop()
+				return
+			default:
 				ft := time.Now().Format(time.RFC3339)
 				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
 					ft = "Some error occured"
 				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+				superChan <- Ttype{id: int(time.Now().Unix()), cT: ft} // передаем таск на выполнение
 			}
-		}()
-	}
-
-	superChan := make(chan Ttype, 10)
-
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
 		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
+	}()
 
 	doneTasks := make(chan Ttype)
 	undoneTasks := make(chan error)
 
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
+	taskSorter := func(t Ttype) {
+		if t.taskRESULT == "task has been successed" {
 			doneTasks <- t
 		} else {
 			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
 		}
 	}
 
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
 		// получение тасков
 		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
+			t = taskWorker(t)
+			// если я правильно понял, то тут происходит обработка тасков и она должна быть асинхронной
+			// оставил как есть, данные продолжат писаться в каналы вывода пока не кончатся в канале superChan
+			go taskSorter(t)
 		}
-		close(superChan)
+		log.Println("Кончились данные в superChan")
 	}()
 
 	result := map[int]Ttype{}
 	err := []error{}
+	wg.Add(1)
 	go func() {
-		for r := range doneTasks {
-			go func() {
+		defer wg.Done()
+		// такой подход позволит получить данные из канала superChan, обработать и сохранить, не должно остаться подвисших горутин
+		for {
+			select {
+			case r := <-doneTasks:
 				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
+			case r := <-undoneTasks:
 				err = append(err, r)
-			}()
+			case <-doneCh:
+				log.Println("Закрыт doneCh вышел из получения результатов")
+				return
+			}
 		}
-		close(doneTasks)
-		close(undoneTasks)
 	}()
 
-	time.Sleep(time.Second * 3)
+	// вывод данных каждые 3 секунды так же по тикеру
+	tickerResult := time.NewTicker(3 * time.Second)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			select {
+			case <-tickerResult.C:
+				log.Println("Errors:")
+				for r := range err {
+					log.Println("Error", r)
+				}
+				log.Println("Done tasks:")
+				for r := range result {
+					log.Println("Done", r)
+				}
+			case <-doneCh:
+				log.Println("Закрыт doneCh вышел из печати результатов")
+				tickerResult.Stop()
+				return
+			}
+		}
+	}()
 
-	println("Errors:")
-	for r := range err {
-		println(r)
-	}
+	time.Sleep(12 * time.Second)
 
-	println("Done tasks:")
-	for r := range result {
-		println(r)
-	}
+	close(doneCh)
+	defer close(doneTasks)
+	defer close(undoneTasks)
+	wg.Wait()
 }
