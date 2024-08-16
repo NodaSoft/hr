0967c5faec7fef5e146c078bf95af35d266764de
Volkov Alexey <Azcarot387@gmail.com>
2024-08-16 13:49:17 +0300

    Тестовое задание

--- golang/main.go
+++ golang/main.go
@@ -1,105 +1,134 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
-// Приложение эмулирует получение и обработку неких тасков. Пытается и получать, и обрабатывать в многопоточном режиме.
-// Приложение должно генерировать таски 10 сек. Каждые 3 секунды должно выводить в консоль результат всех обработанных к этому моменту тасков (отдельно успешные и отдельно с ошибками).
+// type taskData информация о таске
+type taskData struct {
+	id            int
+	createTime    string // время создания
+	executionTime string // время выполнения
+	taskResult    []byte
+}
 
-// ЗАДАНИЕ: сделать из плохого кода хороший и рабочий - as best as you can.
-// Важно сохранить логику появления ошибочных тасков.
-// Важно оставить асинхронные генерацию и обработку тасков.
-// Сделать правильную мультипоточность обработки заданий.
-// Обновленный код отправить через pull-request в github
-// Как видите, никаких привязок к внешним сервисам нет - полный карт-бланш на модификацию кода.
+// type wPool все составляющие воркер пула + каналы для вывода результатов
+type wPool struct {
+	wg               sync.WaitGroup
+	numWorkers       int
+	createdTasksChan chan taskData
+	doneTasks        chan taskData
+	errTasks         chan error
+}
 
-// A Ttype represents a meaninglessness of our life
-type Ttype struct {
-	id         int
-	cT         string // время создания
-	fT         string // время выполнения
-	taskRESULT []byte
+// newWPool - билдер воркер пула
+func newWPool() *wPool {
+	return &wPool{
+		wg:               sync.WaitGroup{},
+		numWorkers:       5,
+		createdTasksChan: make(chan taskData, 10),
+		doneTasks:        make(chan taskData, 10),
+		errTasks:         make(chan error, 10),
+	}
+}
+
+// taskCreator - создание тасков
+func (wp *wPool) taskCreator(ctx context.Context) {
+	defer close(wp.createdTasksChan)
+	defer wp.wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			ft := time.Now().Format(time.RFC3339)
+			if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
+				ft = "Some error occured"
+			}
+			wp.createdTasksChan <- taskData{createTime: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
+		}
+	}
+}
+
+// taskWorkerAndSorter обработка задач и последующая сортировка их результатов по каналам
+func (wp *wPool) taskWorkerAndSorter() {
+	defer wp.wg.Done()
+	for task := range wp.createdTasksChan {
+		tt, err := time.Parse(time.RFC3339, task.createTime)
+		var successFlag bool
+		if err == nil && tt.After(time.Now().Add(-20*time.Second)) {
+			task.taskResult = []byte("task has been successed")
+			successFlag = true
+		} else {
+			task.taskResult = []byte("something went wrong")
+			successFlag = false
+		}
+		task.executionTime = time.Now().Format(time.RFC3339Nano)
+		if successFlag {
+			wp.doneTasks <- task
+		} else {
+			wp.errTasks <- fmt.Errorf("Task id %d time %s, error %s", task.id, task.createTime, task.taskResult)
+		}
+		time.Sleep(time.Millisecond * 150)
+	}
+}
+
+// printResult - вывод результатов выполнения задач
+func (wp *wPool) printResult(ticker *time.Ticker, stopChan <-chan struct{}) {
+	for {
+		select {
+		case <-ticker.C:
+			fmt.Println("Done tasks: ")
+			for len(wp.doneTasks) > 0 {
+				task := <-wp.doneTasks
+				fmt.Printf("Task ID: %d, Created: %s, Finished: %s\n", task.id, task.createTime, task.executionTime)
+
+			}
+			fmt.Println("Errors: ")
+			for len(wp.errTasks) > 0 {
+				err := <-wp.errTasks
+				fmt.Println(err)
+			}
+
+		case <-stopChan:
+			return
+		}
+	}
 }
 
 func main() {
-	taskCreturer := func(a chan Ttype) {
-		go func() {
-			for {
-				ft := time.Now().Format(time.RFC3339)
-				if time.Now().Nanosecond()%2 > 0 { // вот такое условие появления ошибочных тасков
-					ft = "Some error occured"
-				}
-				a <- Ttype{cT: ft, id: int(time.Now().Unix())} // передаем таск на выполнение
-			}
-		}()
+
+	wp := newWPool()
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	stopChan := make(chan struct{})
+
+	wp.wg.Add(1)
+	go wp.taskCreator(ctx)
+
+	//создаем воркер пул
+	for i := 1; i <= wp.numWorkers; i++ {
+		wp.wg.Add(1)
+		go wp.taskWorkerAndSorter()
 	}
 
-	superChan := make(chan Ttype, 10)
+	go wp.printResult(ticker, stopChan)
 
-	go taskCreturer(superChan)
-
-	task_worker := func(a Ttype) Ttype {
-		tt, _ := time.Parse(time.RFC3339, a.cT)
-		if tt.After(time.Now().Add(-20 * time.Second)) {
-			a.taskRESULT = []byte("task has been successed")
-		} else {
-			a.taskRESULT = []byte("something went wrong")
-		}
-		a.fT = time.Now().Format(time.RFC3339Nano)
-
-		time.Sleep(time.Millisecond * 150)
-
-		return a
-	}
-
-	doneTasks := make(chan Ttype)
-	undoneTasks := make(chan error)
-
-	tasksorter := func(t Ttype) {
-		if string(t.taskRESULT[14:]) == "successed" {
-			doneTasks <- t
-		} else {
-			undoneTasks <- fmt.Errorf("Task id %d time %s, error %s", t.id, t.cT, t.taskRESULT)
-		}
-	}
-
-	go func() {
-		// получение тасков
-		for t := range superChan {
-			t = task_worker(t)
-			go tasksorter(t)
-		}
-		close(superChan)
+	wp.wg.Wait()
+	go func() { // Закрываем каналы результатов
+		close(wp.doneTasks)
+		close(wp.errTasks)
+		// Сигнализируем PrintResult завершить работу
+		stopChan <- struct{}{}
+		close(stopChan)
 	}()
 
-	result := map[int]Ttype{}
-	err := []error{}
-	go func() {
-		for r := range doneTasks {
-			go func() {
-				result[r.id] = r
-			}()
-		}
-		for r := range undoneTasks {
-			go func() {
-				err = append(err, r)
-			}()
-		}
-		close(doneTasks)
-		close(undoneTasks)
-	}()
-
-	time.Sleep(time.Second * 3)
-
-	println("Errors:")
-	for r := range err {
-		println(r)
-	}
-
-	println("Done tasks:")
-	for r := range result {
-		println(r)
-	}
+	fmt.Println("All tasks processed, exiting.")
 }
